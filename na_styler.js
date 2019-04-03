35208b4c7d7ebe5204591636f13b2f09053935b6
Unknown <dmedina032@west-mec.org>
2019-04-02 18:29:09 -0700

    Started the code
    Changed the file names and declared variables along with setting their values

---
+++ na_styler.js
@@ -0,0 +1,40 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5, CSS3 and JavaScript 6th Edition
+   Tutorial 12
+   Case Problem 1
+
+   Author: Dajah Medina
+   Date:   4.2.19
+
+   Filename: na_styler.js
+
+   Functions
+   =========
+   
+   setStyles()
+      Sets up the style sheets and the style sheet switcher.
+      
+   randInt(size)
+      Returns a random integer from 0 up to size-1.
+
+*/
+window.addEventListener("load", setStyles);
+
+var styleNum = randlnt(5);
+var linkElem = document.getElementById("link");
+link.setAttribute("rel", "styleSheet");
+link.setAttribute("id", "fancySheet");
+link.setAttribute("href", "na_style_num.css");
+
+document.createElement(figBox);
+figBox
+
+
+
+
+
+function randInt(size) {
+      return Math.floor(size * Math.random());
+}
