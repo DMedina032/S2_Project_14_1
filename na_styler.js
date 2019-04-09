4595fbdfd6af6ba7365b13e9f067b93dbf89841e
Unknown <dmedina032@west-mec.org>
2019-04-09 16:59:31 -0700

    Final commit/submit for review
    Finished my comments

--- na_styler.js
+++ na_styler.js
@@ -20,25 +20,26 @@
       Returns a random integer from 0 up to size-1.
 
 */
+// This event listener will run the setStyles fuction whenever the page is loaded 
 window.addEventListener("load", setStyles);
-
+//This function willl allow a styl sheet to load whnever the page is loaded.
 function setStyles() {
-
+      // decleared the varible styleNum and set it equaled to randInt to be returned to it
       var styleNum = randInt(5);
+      //created an element node and linked it to the style sheet and set each attribute to each style sheet
       var linkElem = document.createElement("link");
       linkElem.setAttribute("rel", "stylesheet");
       linkElem.setAttribute("id", "fancySheet");
       linkElem.setAttribute("href", "na_style_" + styleNum + ".css");
-
+      // Apeend the linkElem to the document head in the style sheet
       document.head.appendChild(linkElem);
-
+      //created the element figBox  so  the users can choose  between each image. For the figBox the attribute was set to it and appened "box" to to the figBox for the div element
       var figBox = document.createElement("figure");
       figBox.setAttribute("id", "styleThumbs");
-
       document.getElementById("box").appendChild(figBox);
 
 
-
+      // made a for loop  that goes  through each image. Each link is the value for the sheetImg varible to loop through each one. Then, the figBox is appendd to the thumbtyles.
       for (var i = 0; i <= 4; i++) {
             var sheetImg = document.createElement("img");
             sheetImg.setAttribute("src", "na_small_" + i + ".png")
@@ -48,11 +49,11 @@ function setStyles() {
             });
             figBox.appendChild(sheetImg);
       }
-
+      // Apped the thumbStyles to the head of the document
       var thumbStyles = document.createElement("style");
       document.head.appendChild(thumbStyles);
 
-
+      // desiging the apperance of the tumbnail figure box 
       document.styleSheets[document.styleSheets.length - 1].insertRule(
             "figure#styleThumbs { \
                   position: absolute; \
@@ -78,7 +79,7 @@ function setStyles() {
 }
 
 
-
+//randInt(size) Returns a random integer from 0 up to size-1.
 function randInt(size) {
       return Math.floor(size * Math.random());
 }
